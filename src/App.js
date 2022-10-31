16f83c80d7e92b83c8e078467758a668d3a3a337
SamGepSal <i_anton94@bk.ru>
2022-11-01 01:41:07 +0300

    lesson-2 test work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,71 @@
-import React from 'react';
-import { Message} from './components/Message';
-import { ClassMessageComp } from './components/ClassMessageComp';
+import React, {useState, useEffect} from 'react';
+// import { Form } from './components/messageList-lesson2/MessageList';
 
 export const App = () => {
   
+  const [messageList, setMessageList] = useState([])
+  const [messageBody, setMessageBody] = useState({
+    text: '',
+    author: ''
+  })
+  
+
+  const RobotMessage = 'Hello! Thank you for your review!'
+
+useEffect(()=>{
+    if (messageList.length > 0 && messageList.slice(-1)[0].author !== 'robot'){
+        setTimeout(()=>{
+            setMessageList(prevstate => [...prevstate, {text: RobotMessage, author: 'robot'}])
+        }, 1500)
+    }
+}, [messageList])
+
   return (
     <div className="App">
-      <Message name = 'Alina'/>
-      <ClassMessageComp />
+      <Form data = {messageBody} setData = {setMessageBody} setMessage = {setMessageList}></Form>
+      <div className='messageList'>
+        {
+          messageList.map((e,i) => <Message text={e.text} author={e.author} key={i}/>)
+        }
+      </div>
     </div>
   );
+}
+
+const Form = ({data, setData, setMessage}) => {
+  
+  const {text, author} = data;
+
+  const submitForm = (e) => {
+      e.preventDefault()
+      if (text.length > 0) {
+          setMessage(prevstate => [...prevstate, {text, author}])
+      }
+      setData({
+          text: '',
+          author: ''
+      })
+  }
+return (
+  <form onSubmit = {submitForm}>
+      <input placeholder='Имя' value={text} onChange = {(e) => 
+      setData(prevstate => ({...prevstate,text: e.target.value}))} />
+
+      <input placeholder='Текст' value={author} onChange = {(e) => 
+      setData(prevstate => ({...prevstate,author: e.target.value}))} />
+
+      <button type='submit'>Send</button>
+  </form>
+)
+
+}
+
+const Message = ({author, text}) => {
+  <div>
+    <hr/>
+    <h1>{text}</h1>
+    <hr/>
+    <p>{author}</p>
+  </div>
 }
 
